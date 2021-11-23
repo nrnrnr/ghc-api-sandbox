cbe75ddbebbf394a77cc8a7ed35fe5c0e3e1936a
Norman Ramsey <nr@cs.tufts.edu>
2021-11-23 16:42:12 -0500

    clean up code layout

--- FastDom.hs
+++ FastDom.hs
@@ -45,11 +45,12 @@ intersectDomSet' :: (DominatorSet -> JoinedFact DominatorSet)
                  -> OldFact DominatorSet
                  -> NewFact DominatorSet
                  -> JoinedFact DominatorSet
-intersectDomSet' nc (OldFact EntryNode)    (NewFact _)         = nc EntryNode
-intersectDomSet' _  (OldFact _)        (NewFact EntryNode)     = Changed EntryNode
-intersectDomSet' nc (OldFact a)        (NewFact AllNodes)     = nc a
-intersectDomSet' _  (OldFact AllNodes)    (NewFact a)         = Changed a
-intersectDomSet' nc ofct@(OldFact (NumberedNode old op)) nfct@(NewFact (NumberedNode new np))
+intersectDomSet' nc (OldFact EntryNode) (NewFact _)         = nc EntryNode
+intersectDomSet' _  (OldFact _)         (NewFact EntryNode) = Changed EntryNode
+intersectDomSet' nc (OldFact a)         (NewFact AllNodes)  = nc a
+intersectDomSet' _  (OldFact AllNodes)  (NewFact a)         = Changed a
+intersectDomSet' nc ofct@(OldFact (NumberedNode old op))
+                    nfct@(NewFact (NumberedNode new np))
   | old < new = intersectDomSet' nc ofct (NewFact np)
   | old > new = intersectDomSet' Changed (OldFact op) nfct
   | otherwise = nc (NumberedNode old op)
