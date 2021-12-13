7e5d0cba2a2600df7be75bf1637ed9e23d383707
Norman Ramsey <nr@cs.tufts.edu>
2021-12-13 11:30:18 -0500

    fix implementation of loop counts

--- GHC/Cmm/LoopInfo.hs
+++ GHC/Cmm/LoopInfo.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module GHC.Cmm.LoopInfo
   ( LoopInfo(..), Edge(..)
   , loopInfo
@@ -9,22 +11,21 @@ import GHC.Cmm.Dataflow.Dominators
 import GHC.Cmm.Dataflow.Graph
 import GHC.Cmm.Dataflow.Label
 
-import GHC.Utils.Panic
-
 data Edge = Edge { edge_from :: Label, edge_to :: Label }
 
 data LoopInfo = LoopInfo
   { liBackEdges :: [Edge] -- ^ List of back edges
   , liHeaders :: LabelSet 
-  , liLevels :: LabelMap Int
-        -- ^ BlockId -> LoopLevel mapping, where loop level is the 
-        -- number of loop headers that (properly?) dominate the block
---  , liLoops :: [(Edge, LabelSet)] -- ^ (backEdge, loopBody), body includes header
+  , liLoops :: [(Edge, LabelSet)] -- ^ Natural loop containing a given back edge;
+                                  -- (backEdge, loopBody) where body includes header
+                                  
+  , liLevels :: LabelMap Int  -- number of natural loops in which need node appears,
+                              -- default 0
   }
 
 
 loopInfo :: NonLocal node => GraphWithDominators node -> LoopInfo
-loopInfo gwd = LoopInfo backEdges headers levels
+loopInfo gwd = LoopInfo backEdges headers loops levels
   where backEdges = [Edge lbl succ | (lbl, b) <- mapToList blockmap,
                                      succ <- successors b,
                                      rpnum succ <= rpnum lbl]
@@ -32,17 +33,31 @@ loopInfo gwd = LoopInfo backEdges headers levels
         l `dominates` l' = l == l' || dominatorsMember l (gwdDominatorsOf gwd l')
         headers = setFromList [succ | Edge lbl succ <- backEdges, succ `dominates` lbl]
 
-        levels = mapMap headerCount $ gwd_dominators gwd
-        headerCount EntryNode = 0
-        headerCount AllNodes = panic "AllNodes in dominator set"
-        headerCount (NumberedNode _ lbl parent) =
-            (if setMember lbl headers then 1 else 0) + headerCount parent
+        predmap :: LabelMap [Label]
+        predmap = mapFoldlWithKey addBlock mapEmpty blockmap
+            where addBlock pm l b =
+                      foldl (\pm succ -> mapAlter cons_l succ pm) pm (successors b)
+                    where cons_l Nothing = Just [l]
+                          cons_l (Just ls) = Just (l : ls)
+
+        preds :: Label -> [Label]
+        preds l = mapFindWithDefault [] l predmap
+
+        naturalLoopOf :: Edge -> LabelSet 
+        naturalLoopOf (Edge from to) = visitPreds from (setSingleton to)
+          where visitPreds lbl visited
+                    | setMember lbl visited = visited
+                    | otherwise = visitNodes (preds lbl) (setInsert lbl visited)
+                visitNodes [] visited = visited
+                visitNodes (l:ls) visited = visitNodes ls (visitPreds l visited)
+        loops = map (\e -> (e, naturalLoopOf e)) backEdges                                          
+        levels = foldl addLoop mapEmpty loops
+          where addLoop labelmap (_, labels) = setFoldl bump labelmap labels
+                bump labelmap label = mapInsert label (succ $ count label labelmap) labelmap
+                count = mapFindWithDefault 0
 
         blockmap = graphMap (gwd_graph gwd)
         rpnum = gwdRPNumber gwd
-
-
-
 
 -- A node is a loop header if it is a target 
 -- of a back edge from a node it dominates.
