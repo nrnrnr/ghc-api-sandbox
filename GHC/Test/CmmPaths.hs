47ce483c3e9ab431d194f24537b8966caf4c9859
Norman Ramsey <nr@cs.tufts.edu>
2021-12-13 16:27:01 -0500

    add module to enumerate Cmm paths

---
+++ GHC/Test/CmmPaths.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE GADTs #-}
+
+module GHC.Test.CmmPaths
+where
+
+import Prelude hiding (succ)
+
+import GHC.Cmm
+import GHC.Cmm.Dataflow.Block
+import GHC.Cmm.Dataflow.Collections
+import GHC.Cmm.Dataflow.Graph
+import GHC.Cmm.Dataflow.Label
+import GHC.Cmm.Switch
+
+import GHC.Test.ControlMonad
+
+import GHC.Utils.Panic
+
+type Path' = [Event]
+
+eventPaths :: CmmGraph ->  [[Event]]
+eventPaths g = map reverse $ pathsPrefixed (g_entry g) [] setEmpty
+  where pathsPrefixed :: Label -> Path' -> LabelSet -> [Path']
+            -- ^ returns a list of all _short_ paths that begin with (block : prefix),
+            -- where a short path is one that contains at most one repeated label,
+            -- which must be the last one on the path (and so at the head of the list).
+            -- Precondition: `visited == setFromList prefix`.
+        pathsPrefixed lbl prefix visited = toLbl : extensions
+          where toLbl = Action lbl : prefix
+                visited' = setInsert lbl visited
+                extensions = if setMember lbl visited then []
+                             else concat [pathsPrefixed s toLbl visited' |
+                                            s <- successors (blockLabeled lbl)]
+
+        blockLabeled lbl = mapFindWithDefault (panic "missing block") lbl blockmap
+
+
+        CmmGraph { g_graph = GMany NothingO blockmap NothingO } = g
+
+
+exits :: CmmBlock -> [(Maybe Event, Label)]
+exits b =
+    case lastNode b of
+      CmmBranch l -> [(Nothing, l)]
+      CmmCondBranch _ t f _ -> [(Just $ Predicate blabel True, t), (Just $ Predicate blabel False, f)]
+      CmmSwitch _ targets ->
+          let (lo, hi) = switchTargetsRange targets
+              dests = switchTargetsCases targets
+              other = switchTargetsDefault targets
+              labelOf i = case [lbl | (j, lbl) <- dests, j == i]
+                          of [lbl] -> lbl
+                             [] -> case other of
+                                     Just lbl -> lbl
+                                     Nothing -> panic "GHC.Tests.CmmPaths.exit: no default"
+                             (_ : _ : _) -> panic "GHC.Tests.CmmPaths.exit: too many matches"
+          in  [(Just $ Switch blabel (lo, hi) i, labelOf i) | i <- [lo..hi]]
+
+          
+      CmmCall { cml_cont = Just l } -> [(Nothing, l)]
+      CmmCall { cml_cont = Nothing } -> []
+      CmmForeignCall { succ = l } -> [(Nothing, l)]
+  where blabel = entryLabel b
