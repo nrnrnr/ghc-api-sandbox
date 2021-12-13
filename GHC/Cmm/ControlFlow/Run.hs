0f8d49f456b876ab90f922313a6227143841e896
Norman Ramsey <nr@cs.tufts.edu>
2021-12-13 16:26:53 -0500

    add module to run Cmm control flow

---
+++ GHC/Cmm/ControlFlow/Run.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module GHC.Cmm.ControlFlow.Run
+  ( eval
+  )
+where
+
+import Prelude hiding (succ)
+
+import GHC.Cmm
+import GHC.Cmm.Dataflow.Block
+import GHC.Cmm.Dataflow.Collections
+import GHC.Cmm.Dataflow.Graph
+import GHC.Cmm.Dataflow.Label
+import GHC.Cmm.Switch
+
+import GHC.Test.ControlMonad
+
+import GHC.Utils.Panic
+
+
+eval :: forall m . ControlTestMonad m => CmmGraph -> m ()
+eval g = run (g_entry g)
+  where GMany NothingO blockmap NothingO = g_graph g
+        run :: Label -> m ()
+        run label = do
+          takeAction label
+          case lastNode (blockOf label) of
+            CmmBranch l -> run l
+            CmmCondBranch _ t f _ -> do
+                      b <- evalPredicate label
+                      run (if b then t else f)
+            CmmSwitch _ targets -> do
+                      i <- evalEnum label $ extendRight $ switchTargetsRange targets
+                      run $ labelIn i targets
+                
+            CmmCall { cml_cont = Just l } -> run l
+            CmmCall { cml_cont = Nothing } -> return ()
+            CmmForeignCall { succ = l } -> run l
+
+        blockOf lbl =
+             mapFindWithDefault (panic "GHC.Cmm.ControlFlow.Run.eval") lbl blockmap
+
+extendRight :: Integral n => (n, n) -> (n, n)
+extendRight (lo, hi) = (lo, hi + 1)
+
+labelIn :: Integer -> SwitchTargets -> Label
+labelIn i targets =
+    case [lbl | (j, lbl) <- switchTargetsCases targets, j == i]
+      of [lbl] -> lbl
+         [] -> case switchTargetsDefault targets of
+                 Just lbl -> lbl
+                 Nothing -> panic "GHC.Cmm.ControlFlow.Run.labelIn: no default"
+         (_ : _ : _) -> panic "GHC.Cmm.ControlFlow.Run: too many matches"
