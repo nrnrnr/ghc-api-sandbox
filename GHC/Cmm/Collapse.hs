baaf31a52e6afa0d967306bebb6a900faf62129d
Norman Ramsey <nr@cs.tufts.edu>
2022-02-07 17:14:19 -0500

    GHC.Cmm.Collapse compiles

--- GHC/Cmm/Collapse.hs
+++ GHC/Cmm/Collapse.hs
@@ -5,33 +5,46 @@
 {-# LANGUAGE FlexibleInstances #-}
 
 module GHC.Cmm.Collapse
---  ( collapseCmm
---  )
+  ( collapseCmm
+  )
 where
 
+
+
 import Control.Monad.State
+import Data.List
 import Data.Maybe
 
+
+
 import GHC.Cmm
+import GHC.Cmm.Dataflow.Collections
+import GHC.Cmm.Dataflow.Graph
+import GHC.Cmm.Dataflow.Label
+import GHC.Cmm.Dominators
 
 import qualified GHC.Data.Graph.Inductive.PatriciaTree as PT
 import qualified GHC.Data.Graph.Inductive as IG
-import GHC.Wasm.ControlFlow.Collapse (Info(..), VizMonad(..))
+import GHC.Wasm.ControlFlow.Collapse (Info(..), VizMonad(..), collapse)
 
 
 cgraphOfCmm :: CmmGraph -> CGraph
-cgraphOfCmm g = foldl' addSuccEdges (mkGraph cnodes []) cnodes
-   where cnodes = zipWith labelNode [0..] (revPostorderFrom blockmap (g_entry g))
-         labelNode k block = (k, info)
-          where info = Info { unsplitLabels = setSingleton (entryLabel block)
-                            , splitLabels = setEmpty
-                            , rpnumber = k
-                            }
-         addSuccEdges graph block = insEdges undefined
-
-graphMap :: GenCmmGraph n -> LabelMap (Block n C C)
-graphMap (CmmGraph { g_graph = GMany NothingO blockmap NothingO }) = blockmap
-
+cgraphOfCmm g = foldl' addSuccEdges (IG.mkGraph cnodes []) blocks
+   where blocks = zip [0..] $ revPostorderFrom (graphMap g) (g_entry g)
+         cnodes = [(k, info block) | (k, block) <- blocks]
+          where info block = Info { unsplitLabels = setSingleton (entryLabel block)
+                                  , splitLabels = setEmpty
+                                  , rpnumber = rpnum (entryLabel block)
+                                  }
+         labelNumber = \lbl -> fromJust $ mapLookup lbl numbers
+             where numbers :: LabelMap Int
+                   numbers = mapFromList $ map swap blocks
+                   swap (k, block) = (entryLabel block, k)
+         addSuccEdges :: CGraph -> IG.LNode CmmBlock -> CGraph
+         addSuccEdges graph (k, block) =
+             IG.insEdges [(k, labelNumber lbl, ()) | lbl <- successors block] graph
+         rpnums = gwd_rpnumbering $ graphWithDominators g
+         rpnum lbl = fromJust $ mapLookup lbl rpnums
 
 
 
@@ -39,14 +52,23 @@ graphMap (CmmGraph { g_graph = GMany NothingO blockmap NothingO }) = blockmap
 type CGraph = PT.Gr Info ()
 
 collapseCmm :: CmmGraph -> [(CGraph, IG.LNode Info, CGraph)]
-collapseCmm = undefined
+collapseCmm = unViz . collapse . cgraphOfCmm
 
-newtype VM a = VM { unVM :: State VS a }
+unViz :: State VS a -> [(CGraph, IG.LNode Info, CGraph)]
+unViz m = case execState m emptyVs of
+            VS { initg = Nothing, split_node = Nothing, finals = fs } -> fs
+            _ -> error "viz monad in bad state"
+
+
+newtype VM a = VM { _unVM :: State VS a }
   deriving (Applicative, Functor, Monad)
 
 data VS = VS { initg :: Maybe CGraph, split_node :: Maybe (IG.LNode Info)
              , finals :: [(CGraph, IG.LNode Info, CGraph)]
              }
+
+emptyVs :: VS
+emptyVs = VS Nothing Nothing []
 
 instance MonadState VS VM where
   get = VM get
@@ -59,10 +81,9 @@ instance VizMonad (State VS) PT.Gr where
       modify (\s -> if isJust (initg s) && isNothing (split_node s) then
                         s { split_node = Just ln }
                     else error "double split")
-  finalGraph g' = 
+  finalGraph g' =
       modify (\case VS { initg = Just g, split_node = Just n, finals = fs } ->
                         VS { initg = Nothing, split_node = Nothing
                            , finals = (g, n, g') : fs
                            }
                     _ -> error "final too early")
-                  
