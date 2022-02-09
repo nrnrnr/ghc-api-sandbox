c1b6aa5bdad8a43b05ed1b9bf8961b27f76731dd
Norman Ramsey <nr@cs.tufts.edu>
2022-02-09 14:18:06 -0500

    define function splitAt

--- GHC/Cmm/Reducibility.hs
+++ GHC/Cmm/Reducibility.hs
@@ -1,26 +1,39 @@
+{-# LANGUAGE GADTs #-}
+
 module GHC.Cmm.Reducibility
   ( Reducibility(..)
   , reducibility
-    
+
   , asReducible
 
 
   , Node(..) -- temporary
+
+  , splitAt -- temporary
 
   )
 where
 
 -- see Note [Reducibility resources]
 
-import Data.List
+import Prelude hiding (splitAt, succ)
 
+import Data.List hiding (splitAt)
+
+
+import Control.Monad.State
 
 import GHC.Utils.Panic
 
+import GHC.Cmm
+import GHC.Cmm.BlockId
 import GHC.Cmm.Dataflow
+import GHC.Cmm.Dataflow.Collections
+import GHC.Cmm.Dataflow.Block
 import GHC.Cmm.Dominators
 import GHC.Cmm.Dataflow.Graph
 import GHC.Cmm.Dataflow.Label
+import GHC.Cmm.Switch
 
 import GHC.Types.Unique.Supply
 
@@ -40,7 +53,7 @@ reducibility gwd =
         dominators = gwdDominatorsOf gwd
         dominates lbl blockname =
             lbl == blockname || dominatorsMember lbl (dominators blockname)
-            
+
 
 asReducible :: NonLocal node
             => GraphWithDominators node
@@ -59,6 +72,54 @@ nodeSplit = panic "GHC.Cmm.Reducibility: nodesplit unimp" $ swallow
   3. Split the original graph
   4. call asReducible again
 -}
+
+type BlockMap = LabelMap (Block CmmNode C C)
+
+type UpdateMonad a = StateT BlockMap UniqSM a
+
+splitAt :: LabelMap [Label]
+        -> CmmGraph
+        -> Label
+        -> UpdateMonad CmmGraph
+splitAt predmap g lbl = do
+    new_entry <- if g_entry g /= lbl then return $ g_entry g
+                 else replicate (g_entry g)
+    mapM_ replaceIn (predecessors lbl)
+    blockmap <- get
+    return $ CmmGraph new_entry (GMany NothingO blockmap NothingO)
+   where replicate :: Label -> UpdateMonad Label
+         replicate lbl = do b <- blockLabeled lbl <$> get
+                            lbl' <- lift newBlockId
+                            let b' = relabel lbl' b
+                            modify $ addBlock b'
+                            return lbl'
+         replaceIn plabel = do
+             b <- blockLabeled plabel <$> get
+             new <- replicate lbl
+             modify $ addBlock $ replaceSuccessor lbl new b
+
+         predecessors lbl = mapFindWithDefault (panic "GHC.Cmm.Reducibility") lbl predmap
+
+replaceSuccessor :: Label -> Label -> CmmBlock -> CmmBlock
+replaceSuccessor old new block = blockJoinTail head tail'
+  where (head, tail) = blockSplitTail block
+        tail' = update tail
+        redirect lbl = if lbl == old then new else lbl
+        single lbl = if lbl == old then new else panic "GHC.Cmm.Reducibility.replaceSuccessor"
+        update (CmmBranch lbl) = CmmBranch (single lbl)
+        update (CmmCondBranch e t f likely) = CmmCondBranch e (redirect t) (redirect f) likely
+        update (CmmSwitch e targets) = CmmSwitch e (mapSwitchTargets redirect targets)
+        update (c@CmmCall {cml_cont = succ}) = c { cml_cont = fmap single succ }
+        update (c@CmmForeignCall {succ = s}) = c { succ = single s }
+
+
+
+blockLabeled :: Label -> BlockMap -> CmmBlock
+blockLabeled = mapFindWithDefault (panic "GHC.Cmm.Reducibility")
+
+relabel :: Label -> CmmBlock -> CmmBlock
+relabel lbl b = blockJoinHead (CmmEntry lbl scope) tail
+  where (CmmEntry _ scope, tail) = blockSplitHead b
 
 data XLabel = Original Label
             | Replica Int XLabel
@@ -87,7 +148,7 @@ swallow u v =
 Available to borrow from archive.org.
 
 Matthew S. Hecht and Jeffrey D. Ullman (1972).
-Flow Graph Reducibility. SIAM J. Comput., 1(2), 188–202. 
+Flow Graph Reducibility. SIAM J. Comput., 1(2), 188–202.
 https://doi.org/10.1137/0201014
 
 Johan Janssen and Henk Corporaal. 1997. Making graphs reducible with
