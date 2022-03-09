b59c2b9be1df3453d281295018540da45148833b
Norman Ramsey <nr@cs.tufts.edu>
2022-03-09 18:07:43 -0500

    add PTest.hs which runs all tests

--- Placeholder.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import GHC.Cmm.Dominators()
-import GHC.Data.Graph.Inductive.PatriciaTree()
-import GHC.Cmm.Reducibility()
-import GHC.Wasm.ControlFlow.FromCmm()
-
-main :: IO ()
-main = putStrLn "Nothing to see here.  Move along."
