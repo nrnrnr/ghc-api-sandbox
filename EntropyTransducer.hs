2e6cf86e1d6a61e6733938491080e892946cf18b
Norman Ramsey <nr@cs.tufts.edu>
2022-03-30 17:02:28 -0400

    add entropy transducers

---
+++ EntropyTransducer.hs
@@ -0,0 +1,31 @@
+module EntropyTransducer
+  ( traceBits
+  , rangeSelect
+  )
+where
+
+import ControlTestMonad
+
+import GHC.Utils.Panic
+
+traceBits :: [Event a b] -> [Bool]
+traceBits (Predicate _ b : events) = b : traceBits events
+traceBits (Action _ : events) = traceBits events
+traceBits (Switch _ (lo, hi) i : events) =
+    inverseRangeSelect (lo, hi) i ++ traceBits events
+traceBits [] = []
+
+
+rangeSelect :: (Integer, Integer) -> [Bool] -> Maybe (Integer, [Bool])
+rangeSelect (lo, limit) bits | lo == pred limit = Just (lo, bits)
+rangeSelect _ [] = Nothing
+rangeSelect (lo, limit) (bit : bits) =
+    rangeSelect (if bit then (lo, mid) else (mid, limit)) bits
+  where mid = (lo + limit) `div` 2
+
+inverseRangeSelect :: (Integer, Integer) -> Integer -> [Bool]
+inverseRangeSelect (lo, limit) i
+    | lo == pred limit = if i == lo then [] else panic "inverseRangeSelect"
+    | otherwise = if i < mid then True : inverseRangeSelect (lo, mid) i
+                  else False : inverseRangeSelect (mid, limit) i
+  where mid = (lo + limit) `div` 2
